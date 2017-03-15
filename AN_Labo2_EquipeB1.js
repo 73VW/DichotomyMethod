490510d3c770b63dec498090bd6f8f20e8fa22ed
axel.rieben <axel.rieben@he-arc.ch>
2017-03-15 22:06:40 +0100

    Add raw algorithm, some html and css

--- AN_Labo2_EquipeB1.js
+++ AN_Labo2_EquipeB1.js
@@ -1,16 +1,25 @@
-/*********************************************/
-/*  Tools : Easiest way to get element by id  */
-/**********************************************/
+/*************************************************************/
+/*  Auhor : Axel Rieben, Maël Pedretti, Quentin Vaucher		 */
+/*	Date : 28 March 2017                                     */
+/*************************************************************/
+
+/*************************************************************/
+/*  Tools : Easiest way to get element by id                 */
+/*************************************************************/
 
 function $(id) {
   return document.getElementById(id);
 }
 
-/************************************************/
-/*  Paint device, paint context and dimensions  */
-/************************************************/
+function $name(name) {
+  return document.getElementsByName(name);
+}
 
-var canvas = document.getElementById('canvas');
+/*************************************************************/
+/*  Paint device, paint context and dimensions               */
+/*************************************************************/
+
+var canvas = $('canvas');
 var context = canvas.getContext('2d');
 var width = canvas.width;
 var height = canvas.height;
@@ -51,9 +60,9 @@ var plot = function plot(fn, range) {
   context.stroke();
 };
 
-/*******************/
-/*  Function calls */
-/*******************/
+/*************************************************************/
+/*  Function calls                                           */
+/*************************************************************/
 
 // Function 1
 plot(function (x) {
@@ -64,3 +73,37 @@ plot(function (x) {
 // plot(function (x) {
 //   return x / (1 - x*x);
 // }, [0, Math.PI * 4, -4, 4]);
+
+/*************************************************************/
+/*  Dichotomy functions                                      */
+/*************************************************************/
+
+//a : range left, b : range right, f : function to solve, a < b
+function dichotomy(a, b, f) {
+	fa = f(a);
+	mnew = a + b;
+	mold = 2 * mnew;
+	
+	while((mnew - mold) != 0) {
+		mold = mnew;
+		mnew = (a + b) / 2;
+		fm = f(mnew);
+		
+		if(fm * fa <= 0) {
+			b = mnew;
+		} else {
+			a = mnew;
+			fa = fm;
+		}
+	}
+	
+	return mnew;
+}
+
+/*************************************************************/
+/*  User interactions	                                     */
+/*************************************************************/
+
+function solve() {
+	
+}
