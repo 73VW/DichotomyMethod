6716890239600163c8da33fd3010b73f87144c98
QVaucher <quentin.vaucher@protonmail.com>
2017-03-25 18:49:14 +0100

    Rewrite completely JS in order to use plotly.js

--- AN_Labo2_EquipeB1.js
+++ AN_Labo2_EquipeB1.js
@@ -15,95 +15,54 @@ function $name(name) {
   return document.getElementsByName(name);
 }
 
-/**********************************************************/
-/*  Paint device, paint context, viewportSize, axis, ...  */
-/**********************************************************/
+/**************************************/
+/*  Plotly.js : Plot, show axis, ...  */
+/**************************************/
 
-var canvas = $('canvas');
-var context = canvas.getContext('2d');
-var viewportSize = [canvas.width, canvas.height];
-
-/********************/
-/*  Plot Functions  */
-/********************/
-
-// Convert a point from the world coordinates system (i.e. -100; 100) to the viewport coordinates system (i.e. canvas dimensions) and return it.
-// point: A two values array [x, y] which represent the point to convert.
-// windowRange: A four values array [xMin, xMax, yMin, yMax] which represent the range of the world coordinates system.
-// viewportSize: A two values array [width, height] which represent the dimensions of the viewport coordinates system.
-function windowToViewportConversion(point, windowRange) {
-  var windowSize = [(windowRange[1] - windowRange[0]), (windowRange[3] - windowRange[2])];
-
-  point[0] = ((viewportSize[0] / windowSize[0] * point[0]) + viewportSize[0] / 2); // x conversion
-  point[1] = ((-viewportSize[1] / windowSize[1] * point[1]) + viewportSize[1] / 2); // y conversion, "-" because (0, 0) is in top left corner
-
-  return point;
-}
-
-// Draw axis acording to the specified range.
-// windowRange: A four values array [xMin, xMax, yMin, yMax] which represent the range of the world coordinates system.
-function drawAxis(windowRange) {
-  context.beginPath();
-
-  // x-axis
-  context.moveTo(0, canvas.height/2);
-  context.lineTo(canvas.width, canvas.height/2);
-
-  // y-axis
-  context.moveTo(canvas.width/2, 0);
-  context.lineTo(canvas.width/2, canvas.height);
-
-  // Appearance
-  {
-    context.strokeStyle = "black";
-    context.lineWidth = 5;
-    context.stroke();
-  }
-
-  context.closePath();
-}
-
-// Move to the first point to draw without drawing anything.
-// fn: Function to draw.
-// windowRange: A four values array [xMin, xMax, yMin, yMax] which represent the range of the world coordinates system.
-function moveToStartPoint(fn, windowRange) {
-  var startPoint = [windowRange[0], fn(windowRange[0])];
-  startPoint = windowToViewportConversion(startPoint, windowRange, viewportSize);
-
-  context.moveTo(startPoint[0], startPoint[1]);
-}
-
-// Plot the specified function inside the specified range.
-// fn: Function to draw;
-// windowRange: A four values array [xMin, xMax, yMin, yMax] which represent the range of the world coordinates system.
-function plot(fn, windowRange) {
-  drawAxis(windowRange);
-  context.beginPath();
-
-  moveToStartPoint(fn, windowRange);
-
-  var point;
-
-  for (let x = windowRange[0]; x <= windowRange[1]; x+=0.02) {
-    point = [x, fn(x)];
-    point = windowToViewportConversion(point, windowRange);
-
-    if (point[1] < 0 || point[1] > canvas.height) {  // Avoid ugly line between asymptotes
-      context.moveTo(point[0], point[1]);
-    } else {
-      context.lineTo(point[0], point[1]);
+// Create readable data for Plotly and style them a little.
+// Input: The list of x values and y values + the name of the line
+function creatingData(listPoints, name) {
+  var data = {
+    x: listPoints[0],
+    y: listPoints[1],
+    mode: 'lines',
+    name: name,
+    marker: {
+      color: 'rgb(41, 128, 185)'
+    },
+    line: {
+      width: 4
     }
-  }
-
-  // Appearance
-  {
-    context.strokeStyle = "red";
-    context.lineWidth = 5;
-    context.stroke();
-  }
-  context.closePath();
+  };
+  return data
 }
 
+// Plot the graph according to the given points
+// Input: list of x values and y values
+function plot(data) {
+  // Everything which is related to style
+  var layout = {
+    font: {
+      family: 'Arial, sans-serif',
+      size: 20
+    },
+    xaxis: {
+      range: [-100,100],
+      title: 'x',
+      zeroline: true,
+      zerolinewidth: 3
+    },
+    yaxis: {
+      range: [-20,20],
+      title: 'f(x)',
+      zeroline: true,
+      zerolinewidth: 4
+    },
+    title: "Dichotomy method",
+  };
+
+  Plotly.newPlot($('plotly'), data, layout);
+}
 
 /***************/
 /*  Functions  */
@@ -117,6 +76,76 @@ function f1(x) {
 // Function 2
 function f2(x) {
   return x / (1 - x*x);
+}
+
+/***********************/
+/*  Points generation  */
+/***********************/
+
+// Generate the points between start and stop, according to fn.
+function generatePointsToDraw(fn, start, stop) {
+  var xValues = [];
+  var yValues = [];
+  var index = 0;
+  for (let i = start; i <= stop; i+= 0.02) {
+    xValues[index] = i;
+    yValues[index] = fn(i);
+    index++;
+  }
+  return [xValues, yValues];
+}
+
+/***********************/
+/*  HTML interactions	 */
+/***********************/
+
+// Launch the plotting according to which function was chosen.
+function solve() {
+  var data = [];
+  if ($('f1').checked) {
+    var listPoints = generatePointsToDraw(f1, -100, 100);
+    data[0] = creatingData(listPoints, "f1");
+    loopDichotomy(-100, 100, f1); //BAD RANGE !!!
+  } else { // f2 is plotted in three lines because of the asymptotes
+    var listPoints1 = generatePointsToDraw(f2, -100, -1);
+    var listPoints2 = generatePointsToDraw(f2, -1, 1);
+    var listPoints3 = generatePointsToDraw(f2, 1, 100);
+
+    data[0] = creatingData(listPoints1, '[-100, -1]');
+    data[1] = creatingData(listPoints2, '[-1, 1]');
+    data[2] = creatingData(listPoints3, '[1, 100]');
+
+    let listResults = dichotomy(-101, 100, f2); //BAD RANGE !!!
+    printSolution(listResults);
+  }
+  plot(data);
+}
+
+//Print the solution
+function printSolution(listResults)
+{
+  let div = $('result');
+  div.innerHTML = "";
+  div.innerHTML += "Root(s) of the function : {";
+  div.innerHTML += listResults;
+  /*
+  for (let result of listResults)
+  {
+    div.innerHTML += result + "; ";
+  }
+
+  div.innerHTML = div.innerHTML.substring(0, div.innerHTML.length-2); //Remove the last ;
+  */
+  div.innerHTML += "}";
+
+}
+
+//Print error
+function printError(error)
+{
+    let div = $('error');
+    div.innerHTML = "";
+    div.innerHTML = "Error approximation : " + error;
 }
 
 /*************************************************************/
@@ -166,48 +195,4 @@ function dichotomy(a, b, f) {
 function calculateError(a, b, n)
 {
     return (b - a) / (2 * Math.pow(2, n)); //for each step the error is diminished by half
-}
-
-/***********************/
-/*  HTML interactions	 */
-/***********************/
-
-function solve() {
-  context.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
-
-  if ($('f1').checked) {
-    plot(f1, [-100, 100, -10, 10]);
-    loopDichotomy(-101, 100, f1); //BAD RANGE !!!
-  } else {
-    plot(f2, [-10, 10, -1.4, 1.4]);
-    let listResults = dichotomy(-101, 100, f2); //BAD RANGE !!!
-    printSolution(listResults);
-  }
-}
-
-//Print the solution
-function printSolution(listResults)
-{
-  let div = $('result');
-  div.innerHTML = "";
-  div.innerHTML += "Root(s) of the function : {";
-  div.innerHTML += listResults;
-  /*
-  for (let result of listResults)
-  {
-    div.innerHTML += result + "; ";
-  }
-
-  div.innerHTML = div.innerHTML.substring(0, div.innerHTML.length-2); //Remove the last ;
-  */
-  div.innerHTML += "}";
-
-}
-
-//Print error
-function printError(error)
-{
-    let div = $('error');
-    div.innerHTML = "";
-    div.innerHTML = "Error approximation : " + error;
 }
